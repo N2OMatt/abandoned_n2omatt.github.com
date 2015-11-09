a5f013344d1dc4929217b67e9275b3c7e3e3dcaf
n2omatt <n2o.matt@gmail.com>
2015-11-09 13:44:39 -0200

    Add post Why I prefer two separated... and it's code

---
+++ code/2015-11-09-why-i-prefer-two-separated-files-like-c++/main.cpp
@@ -0,0 +1,16 @@
+//THIS CODE IS UNDER THE GPLv3 LICENSE 
+//Check the LICENSE file in the codes top folder.
+//Author: n2omatt - n2o.matt@gmail.com
+//Enjoy...
+
+#include "SpaceShip.h"
+
+int main()
+{
+	//Our main class...
+	SpaceShip spaceShip;
+
+	//Use it's public stuff...
+	//It will delegate the stuff to SpaceShipPrivate class.
+	spaceShip.killAllUglyAliens();
+}
